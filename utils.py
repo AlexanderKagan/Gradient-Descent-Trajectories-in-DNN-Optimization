113cb0f8dfa492745501a50c29c034b09133bb85
AlexanderKagan <44706384+AlexanderKagan@users.noreply.github.com>
2020-12-17 06:26:37 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
+import torch
+from torch.distributions import MultivariateNormal
+
+
 def tensor_list_prod(tensor_list):
     product = tensor_list[0].T
     for i in range(1, len(tensor_list)):
         product = product @ tensor_list[i].T
     return product
+
+def make_random_affine_dependency_dataset(num_points, x_size, y_size, device):
+
+    R = torch.randn(size=(x_size, y_size))
+    
+    mn = MultivariateNormal(torch.zeros(x_size), torch.eye(x_size))
+    x = mn.sample((num_points,))
+
+    # x = torch.linspace(-10, 10, num_points).unsqueeze(1)
+    y = (x @ R).to(device)
+    x = x.to(device)
+    return (list(zip(x, y)), R)
